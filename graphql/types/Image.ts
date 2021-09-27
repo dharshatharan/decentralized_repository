8c1c217dd2b41c1acfb2e9e99261d01206713f34
dharshatharan <dharshatharan@gmail.com>
2021-09-26 21:39:02 -0400

    Add Image Mutation

--- graphql/types/Image.ts
+++ graphql/types/Image.ts
@@ -1,11 +1,32 @@
-import { objectType, extendType, intArg, stringArg } from 'nexus';
+import { Kind } from 'graphql';
+import { objectType, intArg, nonNull, mutationField, queryField, arg, inputObjectType, scalarType } from 'nexus';
+
+const DateScalar = scalarType({
+  name: 'Date',
+  asNexusMethod: 'date',
+  description: 'Date custom scalar type',
+  parseValue(value) {
+    return new Date(value)
+  },
+  serialize(value) {
+    return value.getTime()
+  },
+  parseLiteral(ast) {
+    if (ast.kind === Kind.INT) {
+      return new Date(ast.value)
+    }
+    return null
+  },
+})
+
+export const DateTime = DateScalar
 
 export const Image = objectType({
   name: 'Image',
   definition(t) {
     t.int('id');
-    t.string('createdAt');
-    t.string('updatedAt');
+    t.field('createdAt', { type: DateTime });
+    t.field('updatedAt', { type: DateTime });
     t.string('title');
     t.string('description');
     t.string('url');
@@ -40,71 +61,95 @@ export const Response = objectType({
   },
 });
 
-export const ImagesQuery = extendType({
-  type: "Query",
-  definition(t) {
-    t.field("images", {
-      type: Response,
-      args: {
-        first: intArg(),
-        after: intArg(),
+export const ImagesQuery = queryField("images", {
+  type: Response,
+  args: {
+    first: intArg(),
+    after: intArg(),
+  },
+  async resolve(_, args, ctx) {
+    let queryResults = null;
+
+    if (!args.first) {
+      args.first = 10;
+    }
+
+    if (args.after) {
+      queryResults = await ctx.prisma.image.findMany({
+        take: args.first,
+        skip: 1,
+        cursor: {
+          id: args.after,
+        },
+      });
+    } else {
+      queryResults = await ctx.prisma.image.findMany({
+        take: args.first,
+      });
+    }
+
+    if (queryResults.length > 0) {
+      // last element
+      const lastImageInResults = queryResults[queryResults.length - 1];
+      // cursor we'll return
+      const myCursor = lastImageInResults.id;
+
+      // queries after the cursor to check if we have nextPage
+      const secondQueryResults = await ctx.prisma.image.findMany({
+        take: args.first,
+        cursor: {
+          id: myCursor,
+        },
+      });
+
+      const result = {
+        pageInfo: {
+          endCursor: myCursor.toString(),
+          hasNextPage: secondQueryResults.length >= args.first,
+        },
+        edges: queryResults.map((image: any) => ({
+          cursor: image.id,
+          node: image,
+        })),
+      };
+
+      return result;
+    }
+    return {
+      pageInfo: {
+        endCursor: null,
+        hasNextPage: false,
       },
-      async resolve(_, args, ctx) {
-        let queryResults = null;
+      edges: [],
+    };
+  },
+});
 
-        if (!args.first) {
-          args.first = 10;
-        }
+export const AddImageInput = inputObjectType({
+  name: 'AddImageInput',
+  definition(t) {
+    t.nonNull.string('title')
+    t.string('description')
+    t.nonNull.string('url')
+  },
+})
 
-        if (args.after) {
-          queryResults = await ctx.prisma.image.findMany({
-            take: args.first,
-            skip: 1,
-            cursor: {
-              id: args.after,
-            },
-          });
-        } else {
-          queryResults = await ctx.prisma.image.findMany({
-            take: args.first,
-          });
-        }
-
-        if (queryResults.length > 0) {
-          // last element
-          const lastImageInResults = queryResults[queryResults.length - 1];
-          // cursor we'll return
-          const myCursor = lastImageInResults.id;
-
-          // queries after the cursor to check if we have nextPage
-          const secondQueryResults = await ctx.prisma.image.findMany({
-            take: args.first,
-            cursor: {
-              id: myCursor,
-            },
-          });
-
-          const result = {
-            pageInfo: {
-              endCursor: myCursor.toString(),
-              hasNextPage: secondQueryResults.length >= args.first,
-            },
-            edges: queryResults.map((image: any) => ({
-              cursor: image.id,
-              node: image,
-            })),
-          };
-
-          return result;
-        }
-        return {
-          pageInfo: {
-            endCursor: null,
-            hasNextPage: false,
-          },
-          edges: [],
-        };
+export const ImageMutation = mutationField('addImage', {
+  type: 'Image',
+  args: {
+    data: nonNull(
+      arg({
+        type: 'AddImageInput',
+      }),
+    ),
+  },
+  async resolve(_, args, ctx) {
+    return await ctx.prisma.image.create({
+      data: {
+        title: args.data.title,
+        description: args.data.description ?? '',
+        url: args.data.url,
       },
     });
   },
-});
+})
