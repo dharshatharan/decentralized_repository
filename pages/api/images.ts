415346a6b3b0d13027021ffb066f476c5422fe11
dharshatharan <dharshatharan@gmail.com>
2021-09-27 08:01:59 -0400

    Add IPFS Upload Functionality

--- pages/api/images.ts
+++ pages/api/images.ts
@@ -1,21 +1,21 @@
 import { prisma } from "../../lib/db";
 
 export default async function assetHandler(req: any, res: any) {
-    const { method } = req;
+  const { method } = req;
 
-    switch (method) {
-        case "GET":
-            try {   
-                const images = await prisma.image.findMany();
-                res.status(200).json(images);
-            } catch(e) {
-                console.error("Request error", e);
-                res.status(500).json({ error: "Error fetching images" });
-            }
-            break;
-        default:
-            res.setHeader("Allow", ["GET"]);
-            res.status(405).end(`Method ${method} Not Allowed`);
-            break;
-    }
+  switch (method) {
+    case "GET":
+      try {
+        const images = await prisma.image.findMany();
+        res.status(200).json(images);
+      } catch (e) {
+        console.error("Request error", e);
+        res.status(500).json({ error: "Error fetching images" });
+      }
+      break;
+    default:
+      res.setHeader("Allow", ["GET"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+      break;
+  }
 }
