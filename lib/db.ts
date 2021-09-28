d1e94341e6afa462cdf94acc1e523660ca66d26b
Dharsh <43640229+dharshatharan@users.noreply.github.com>
2021-09-27 21:35:48 -0400

    Fix global variable error

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 
 declare global {
-  var prisma: PrismaClient | undefined
+  namespace NodeJS {
+    interface Global {
+      prisma: any;
+    }
+  }
 }
 
 export const prisma =
